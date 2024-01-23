cc77cacdfca7573122c0a93ed584eee17aeec5b9
carocox <carvcox@gmail.com>
2024-01-23 01:27:17 +0000

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
- *
- * Return: 0 when successfull
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }

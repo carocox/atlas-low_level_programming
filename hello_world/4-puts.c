2353eb2e996388a9c9cd934c4fa546a0fcd5c929
carocox <carvcox@gmail.com>
2024-01-23 01:22:43 +0000

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
  * main - this is the main function
- *
  * Return: always 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
